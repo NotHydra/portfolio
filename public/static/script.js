544538947d9fbc6d20d764a409e8bb9871339db4
NotHydra <rizky.irswanda115@gmail.com>
2024-12-29 01:32:53 +0800

    feat: better scroll system

--- public/static/script.js
+++ public/static/script.js
@@ -1,16 +1,29 @@
-ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".home-text", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".home-img", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".about-img", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-2", { delay: 1000 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-3", { delay: 1200 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".main-btn", { delay: 2000 });
-ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-4", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-5", { delay: 1400 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-6", { delay: 1600 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-7", { delay: 1800 });
-ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".section_title", { delay: 200 });
-ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".section_subtitle", { delay: 200 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".qualification_container", { delay: 1200 });
-ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".qualification_sections", { delay: 1200 });
-ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".portfolio_container", { delay: 200 });
+// ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".about .image", { delay: 200 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".about .text .button", { delay: 2000 });
+
+// ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll", { delay: 200 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-2", { delay: 1000 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-3", { delay: 1200 });
+// ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-4", { delay: 200 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-5", { delay: 1400 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-6", { delay: 1600 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".scroll-7", { delay: 1800 });
+
+// ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".section_title", { delay: 200 });
+// ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".section_subtitle", { delay: 200 });
+
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".qualification_container", { delay: 1200 });
+// ScrollReveal({ origin: "bottom", distance: "61px", duration: 2000, reset: false }).reveal(".qualification_sections", { delay: 1200 });
+
+// ScrollReveal({ origin: "top", distance: "61px", duration: 2000, reset: false }).reveal(".portfolio_container", { delay: 200 });
+
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-200", { delay: 200 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-400", { delay: 400 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-600", { delay: 600 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-800", { delay: 800 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-1000", { delay: 1000 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-1200", { delay: 1200 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-1400", { delay: 1400 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-1600", { delay: 1600 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-1800", { delay: 1800 });
+ScrollReveal({ origin: "bottom", distance: "41px", duration: 2000, reset: false }).reveal(".scroll-bottom-2000", { delay: 2000 });
--- src/components/home/index.component.tsx
+++ src/components/home/index.component.tsx
@@ -7,32 +7,34 @@ export default function Home(): JSX.Element {
     return (
         <section className="home" id="home">
             <div className="text">
-                <h1>
+                <h1 className="scroll-bottom-200">
                     Rizky <span>Irswanda</span>
                 </h1>
 
-                <h6>
+                <h6 className="scroll-bottom-200">
                     Experienced in{" "}
                     <span>
                         <ReactTyped strings={skills} typeSpeed={30} backSpeed={5} loop />
                     </span>
                 </h6>
 
-                <p style={{ textAlign: "justify", fontSize: "1rem", fontWeight: 400 }}>
+                <p className="scroll-bottom-400" style={{ textAlign: "justify", fontSize: "1rem", fontWeight: 400 }}>
                     A second-year <span>Computer Science</span> student who likes to spend time on <span>Software</span> & <span>Internet of Things</span>{" "}
                     Engineering
                 </p>
 
-                <a style={{ marginRight: "1rem" }} href="#about" className="button bigger-margin-right">
-                    Read More
-                </a>
+                <div className="scroll-bottom-600">
+                    <a className="button bigger-margin-right" style={{ marginRight: "1rem" }} href="#about">
+                        Read More
+                    </a>
 
-                <a target="_blank" href="https://drive.google.com/file/d/1f3naCSF5b0VDV5cROJFspEJlFaARyASP/view?usp=sharing" className="button">
-                    View CV
-                </a>
+                    <a className="button" href="https://drive.google.com/file/d/1f3naCSF5b0VDV5cROJFspEJlFaARyASP/view?usp=sharing" target="_blank">
+                        View CV
+                    </a>
+                </div>
             </div>
 
-            <div className="image">
+            <div className="image scroll-bottom-1000">
                 <img src="image/profile.jpg" alt="Profile" />
             </div>
         </section>
