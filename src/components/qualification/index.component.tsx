89f74c2f8e04ae092f12c4d936a23ad50a66d7be
NotHydra <rizky.irswanda115@gmail.com>
2025-01-01 00:28:44 +0800

    feat: remove organization & experience in education

--- src/components/qualification/index.component.tsx
+++ src/components/qualification/index.component.tsx
@@ -250,39 +250,6 @@ export default function Qualification(): JSX.Element {
                         </QualificationTimelineItemDescriptionGroup>
 
                         <QualificationTimelineItemDescriptionGroup>
-                            <QualificationTimelineItemDescriptionPoint text="Organizations & Experiences" />
-
-                            <QualificationTimelineItemDescriptionListDate
-                                items={[
-                                    {
-                                        text: "Member of Ouroboros Technology",
-                                        date: "October 2023 - Present",
-                                    },
-                                    {
-                                        text: "Member of Prototype Division in Department of Mathematics and Information Technology Expo",
-                                        date: "October 2023 - November 2023",
-                                    },
-                                    {
-                                        text: "Head of Prototype Division in Major of Informatics ITK Open House",
-                                        date: "January 2024 - February 2024",
-                                    },
-                                    {
-                                        text: "Lab Assistant for Algorithm & Programming Subject Even Semester 2023/2024",
-                                        date: "February 2024 - May 2024",
-                                    },
-                                    {
-                                        text: "Intern Staff in the Talent Development Department of the Informatics Student Association",
-                                        date: "April 2024 - September 2024",
-                                    },
-                                    {
-                                        text: "Lab Assistant for Algorithm & Programming Subject Odd Semester 2024/2025",
-                                        date: "August 2024 - Present",
-                                    },
-                                ]}
-                            />
-                        </QualificationTimelineItemDescriptionGroup>
-
-                        <QualificationTimelineItemDescriptionGroup>
                             <QualificationTimelineItemDescriptionPoint text="Relevant Courses" />
 
                             <QualificationTimelineItemDescriptionList
