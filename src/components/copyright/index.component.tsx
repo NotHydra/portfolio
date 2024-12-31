eaa0248234a97e17657a2722859672a5e950cf97
NotHydra <rizky.irswanda115@gmail.com>
2024-12-31 19:04:28 +0800

    feat: copyright section animation

--- src/components/copyright/index.component.tsx
+++ src/components/copyright/index.component.tsx
@@ -5,11 +5,11 @@ import CopyrightContactItem from "./contact/item.component";
 export default function Copyright(): JSX.Element {
     return (
         <div className="copyright" id="contact">
-            <h3 style={{ marginBottom: "1rem" }}>
+            <h3 className="scroll-bottom-200" style={{ marginBottom: "1rem" }}>
                 <b>Contact</b>
             </h3>
 
-            <div className="icon">
+            <div className="icon scroll-bottom-200">
                 <CopyrightContactItem icon="github" link="https://github.com/NotHydra" />
 
                 <CopyrightContactItem icon="instagram" link="https://www.instagram.com/rz_irswanda" />
@@ -19,7 +19,7 @@ export default function Copyright(): JSX.Element {
                 <CopyrightContactItem icon="gmail" link="mailto: rizky.irswanda115@gmail.com" />
             </div>
 
-            <p>
+            <p className="scroll-bottom-200">
                 <b>Copyright © 2024 Rizky Irswanda - All Rights Reserved</b>
             </p>
         </div>
