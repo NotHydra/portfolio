bc438fa79484ada026ab8ed3509e800baa023f98
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-12-31 22:28:54 +0800

    fix: properly load font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
+import { NextFont } from "next/dist/compiled/@next/font";
+import { Rubik } from "next/font/google";
+
 import type { AppProps } from "next/app";
 import { JSX } from "react";
 
 import "@/styles/css/globals.css";
 
+const rubik: NextFont = Rubik({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700", "800", "900"],
+    display: "swap"
+});
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <style jsx global>{`
+        html, body {
+          font-family: ${rubik.style.fontFamily};
+        }
+      `}</style>
+            <Component {...pageProps} />
+        </>
+    );
 }
