0fa724b675b081e9b0fe5d7e1ecb584acf6b3ce7
NotHydra <rizky.irswanda115@gmail.com>
2025-01-14 22:26:19 +0800

    fix: subtitle link hover color

--- src/components/qualification/timeline/item/subtitle/link.component.tsx
+++ src/components/qualification/timeline/item/subtitle/link.component.tsx
@@ -2,8 +2,8 @@ import React, { JSX } from "react";
 
 export default function QualificationTimelineItemSubtitleLink({ subtitle, link }: { subtitle: string; link: string }): JSX.Element {
     return (
-        <a href={link} target="_blank">
-            <span className="item-subtitle item-link">{subtitle}</span>
+        <a className="item-subtitle item-link" href={link} target="_blank">
+            {subtitle}
         </a>
     );
 }
